99897a257094906888aec5af36ab7bf13f4f90bc
MartinBurga <mburga.xixv@gmail.com>
2025-04-13 23:12:26 -0500

    Adicion de datos quemados de los equipos restantes de Liga pro Ecuador

--- App_clase3/Repositories/EquipoRepositorie.cs
+++ App_clase3/Repositories/EquipoRepositorie.cs
@@ -14,14 +14,15 @@ namespace App_clase3.Repositories
             List<Equipo> equipos = new List<Equipo>();
             Equipo ldu = new Equipo
             {
-                descripcion = "campeon de libertadores, sudamericana y recopa",
+                descripcion = "Campeon de libertadores, sudamericana y recopa",
                 id = 1,
                 nombre = "Liga de Quito",
                 partidosJugados = 10,
                 partidosGanados = 10,
                 partidosEmpatados = 0,
                 partidosPerdidos = 0
-            };
+            }
+            ;
             calcularPuntaje(ldu);
             equipos.Add(ldu);
 
@@ -38,6 +39,149 @@ namespace App_clase3.Repositories
             };
             calcularPuntaje(Barcelona);
             equipos.Add(Barcelona);
+
+            Equipo IndependienteDelValle = new Equipo
+            {
+                descripcion = "Campeon de la Sudamericana 2019 y 2022",
+                id = 3,
+                nombre = "Independiente del Valle",
+                partidosJugados = 10,
+                partidosGanados = 7,
+                partidosEmpatados = 2,
+                partidosPerdidos = 1
+            };
+            calcularPuntaje(IndependienteDelValle);
+            equipos.Add(IndependienteDelValle);
+
+            Equipo Emelec = new Equipo
+            {
+                descripcion = "Multicampeon nacional, finalista de Libertadores 1995",
+                id = 4,
+                nombre = "Emelec",
+                partidosJugados = 10,
+                partidosGanados = 5,
+                partidosEmpatados = 3,
+                partidosPerdidos = 2
+            };
+            calcularPuntaje(Emelec);
+            equipos.Add(Emelec);
+
+            Equipo ElNacional = new Equipo
+            {
+                descripcion = "Equipo de las Fuerzas Armadas, tricampeon",
+                id = 5,
+                nombre = "El Nacional",
+                partidosJugados = 10,
+                partidosGanados = 4,
+                partidosEmpatados = 4,
+                partidosPerdidos = 2
+            };
+            calcularPuntaje(ElNacional);
+            equipos.Add(ElNacional);
+
+            Equipo DeportivoCuenca = new Equipo
+            {
+                descripcion = "Campeon del 2004",
+                id = 6,
+                nombre = "Deportivo Cuenca",
+                partidosJugados = 10,
+                partidosGanados = 3,
+                partidosEmpatados = 3,
+                partidosPerdidos = 4
+            };
+            calcularPuntaje(DeportivoCuenca);
+            equipos.Add(DeportivoCuenca);
+
+            Equipo Aucas = new Equipo
+            {
+                descripcion = "Campeon 2022 de liga pro",
+                id = 7,
+                nombre = "Aucas",
+                partidosJugados = 10,
+                partidosGanados = 6,
+                partidosEmpatados = 1,
+                partidosPerdidos = 3
+            };
+            calcularPuntaje(Aucas);
+            equipos.Add(Aucas);
+
+            Equipo UniversidadCatolica = new Equipo
+            {
+                descripcion = "Equipo capitalino",
+                id = 8,
+                nombre = "Universidad Catolica",
+                partidosJugados = 10,
+                partidosGanados = 4,
+                partidosEmpatados = 3,
+                partidosPerdidos = 3
+            };
+            calcularPuntaje(UniversidadCatolica);
+            equipos.Add(UniversidadCatolica);
+
+            Equipo Orense = new Equipo
+            {
+                descripcion = "Equipo oriundo de Machala",
+                id = 9,
+                nombre = "Orense",
+                partidosJugados = 10,
+                partidosGanados = 2,
+                partidosEmpatados = 5,
+                partidosPerdidos = 3
+            };
+            calcularPuntaje(Orense);
+            equipos.Add(Orense);
+
+            Equipo Delfin = new Equipo
+            {
+                descripcion = "Campeon 2019",
+                id = 10,
+                nombre = "Delfín",
+                partidosJugados = 10,
+                partidosGanados = 3,
+                partidosEmpatados = 2,
+                partidosPerdidos = 5
+            };
+            calcularPuntaje(Delfin);
+            equipos.Add(Delfin);
+
+            Equipo TecnicoUniversitario = new Equipo
+            {
+                descripcion = "Equipo de Ambato",
+                id = 11,
+                nombre = "Tecnico Universitario",
+                partidosJugados = 10,
+                partidosGanados = 2,
+                partidosEmpatados = 4,
+                partidosPerdidos = 4
+            };
+            calcularPuntaje(TecnicoUniversitario);
+            equipos.Add(TecnicoUniversitario);
+
+            Equipo MushucRuna = new Equipo
+            {
+                descripcion = "Equipo indígena",
+                id = 12,
+                nombre = "Mushuc Runa",
+                partidosJugados = 10,
+                partidosGanados = 3,
+                partidosEmpatados = 2,
+                partidosPerdidos = 5
+            };
+            calcularPuntaje(MushucRuna);
+            equipos.Add(MushucRuna);
+
+            Equipo Imbabura = new Equipo
+            {
+                descripcion = "Equipo de Ibarra",
+                id = 14,
+                nombre = "Imbabura",
+                partidosJugados = 10,
+                partidosGanados = 2,
+                partidosEmpatados = 3,
+                partidosPerdidos = 5
+            };
+            calcularPuntaje(Imbabura);
+            equipos.Add(Imbabura);
             return equipos;
 
 
@@ -71,7 +215,7 @@ namespace App_clase3.Repositories
             var equipoActual = Equipos.FirstOrDefault(e => e.id == id);
             if (equipoActual == null)
             {
-                return false; 
+                return false;
             }
             equipoActual.nombre = equipo.nombre;
             equipoActual.descripcion = equipo.descripcion;
